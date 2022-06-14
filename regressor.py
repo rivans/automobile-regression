9525633a5fc1aac30c3e3a752e180b2afd2d0fd5
Rivansyah Suhendra <mr20rivan@gmail.com>
2022-06-14 13:25:07 +0700

    update regressor.py

--- regressor.py
+++ regressor.py
@@ -2,4 +2,19 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('automobile.csv')
+print(data.describe())
+print(data.head())
+
+X = data.iloc[:,:-1]
+y = data["price"]
+
+model = DecisionTreeRegressor()
+
+X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+model.fit(X_train, y_train)
 
